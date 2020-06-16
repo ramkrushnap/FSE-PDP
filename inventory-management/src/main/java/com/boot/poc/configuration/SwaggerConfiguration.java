dbb5a5c6949f0b19e443fbdb95d543de9436e50b
ctsjavafsd12 <ctsjavafsd12@JavaFSD113.iiht.tech>
2020-06-16 20:07:35 +0530

    Code Refactoring

--- inventory-management/src/main/java/com/boot/poc/configuration/SwaggerConfiguration.java
+++ inventory-management/src/main/java/com/boot/poc/configuration/SwaggerConfiguration.java
@@ -17,22 +17,9 @@ public class SwaggerConfiguration {
 	   @Bean
 	    public Docket swaggerProductApi() { 
 	        return new Docket(DocumentationType.SWAGGER_2)
-	       //   .groupName("product-api-1.0")
 	          .select()                                  
 	          .apis(RequestHandlerSelectors.any())              
 	          .paths(PathSelectors.any())                          
 	          .build();
-	        //  .apiInfo(new ApiInfoBuilder().version("1.0").title("Product Api").description("Documentation for Person API v1.0").build());                                           
 	    }
-//	   
-//	   @Bean
-//	    public Docket swaggerNewProductApi() { 
-//	        return new Docket(DocumentationType.SWAGGER_2)
-//	          .groupName("product-api-1.1")
-//	          .select()                                  
-//	          .apis(RequestHandlerSelectors.any())              
-//	          .paths(regex("/inventory/api/products/v1.1.*"))                          
-//	          .build()
-//	          .apiInfo(new ApiInfoBuilder().version("1.1").title("Product Api").description("Documentation for Person API v1.1").build());                                           
-//	    }
 }
